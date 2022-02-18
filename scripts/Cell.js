6bdb72978a55f771166445b6d448ec74fe325d84
onurk03 <65676529+onurk03@users.noreply.github.com>
2022-02-18 14:57:35 -0600

    Added Win Message, Commenting & Styles
    Added the message displayed when the game is won. Added more clear commenting for classes and methods, changed some styling to make it more responsive.

--- scripts/Cell.js
+++ scripts/Cell.js
@@ -1,3 +1,7 @@
+/**
+ * Represents each cell or light on the board. Stores the total count of cells, the number of
+ * the cell, its state (on/off, 1/0) and location on the board (col, row).
+ */
 class Cell {
     static count = 0;
     constructor(col, row) {
@@ -8,16 +12,28 @@ class Cell {
         this.cellState = Math.round(Math.random());
     }
 
-    setState() {
+    /**
+     * Initializes the rendered cells based on the randomly generated cell state specific to
+     * each cell object. Has to be called on a cell that has already been rendered.
+     */
+    initializeState() {
         if(this.cellState === 0) {
             this.getCell().style.backgroundColor = "black";
         } else {
             this.getCell().style.backgroundColor = "white";
         }
+
+        this.getCell().style.width = "100%";
+        this.getCell().style.height = "100%";
     }
 
+    /**
+     * Flips the state of a cell and turns its background color corresponding to that state.
+     * (1 --> On, white)
+     * (0 --> Off, black)
+     */
     flipState() {
-        if(this.isOn()) {
+        if(this.cellState === 1) {
             this.cellState = 0;
             this.getCell().style.backgroundColor = "black";
         } else {
@@ -26,18 +42,19 @@ class Cell {
         }
     }
 
-    isOn() {
-        if(this.cellState === 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+    /**
+     * Adds the cell on the board.
+     * @returns {string} A string representing the HTML element with a class name
+     * corresponding to the NO of the cell.
+     */
     renderCell() {
         return `<button class="cell${this.cellNo}" type="button"></button>`;
     }
 
+    /**
+     * Finds and returns the element corresponding to this cell object.
+     * @returns {Element} The Cell button element on the DOM corresponding to the NO of the cell.
+     */
     getCell() {
         return document.querySelector(`.cell${this.cellNo}`);
     }
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 let board;
+// screen elements initialized
 let mainScreen = document.querySelector(".main-screen");
+let gameScreen = document.querySelector(".game-screen");
+// button elements initialized
 let choice3x3 = document.querySelector(".option3x3");
 let choice5x5 = document.querySelector(".option5x5");
 let backButton = document.querySelector(".back-button");
-let boardScreen = document.querySelector(".board");
+let resetButton = document.querySelector(".reset-button");
 
 // If user chooses 3x3 board, generate a 3x3 board and change to the game scene.
 choice3x3.addEventListener('click', function() {
@@ -11,7 +14,7 @@ choice3x3.addEventListener('click', function() {
     board = new Board(3,3);
     board.generateBoard();
     startGame();
-    backButton.style.display = "block";
+    gameScreen.style.display = "block";
 });
 
 // If user chooses 5x5 board, generate a 5x5 board and change to the game scene.
@@ -20,15 +23,27 @@ choice5x5.addEventListener('click', function() {
     board = new Board(5,5);
     board.generateBoard();
     startGame();
-    backButton.style.display = "block";
+    gameScreen.style.display = "block";
 });
 
-// If user wants to go back to the main screen, a back arrow button is available.
+// Clicking the back button will reload the page to reset any board that might've been generated
+// and brings the user back to the main screen.
 backButton.addEventListener('click', function() {
     location.reload();
-})
+});
 
-// Adds event listeners to all cells to perform the operation for turning lights on/off.
+// Resets the board and erases the win message.
+resetButton.addEventListener('click', function () {
+    board.resetBoard();
+    board.generateBoard();
+    startGame();
+    document.querySelector(".game-screen h2").style.display = "none";
+});
+
+
+/**
+ * Adds event listeners to all cells to perform the operation for turning lights on/off.
+ */
 function startGame() {
     for (let i = 0; i < board.rows; i++) {
         for (let j = 0; j < board.cols; j++) {
@@ -47,6 +62,12 @@ function startGame() {
                 }
                 if (cell.col > 0) {
                     board.boardArray[cell.row][cell.col - 1].flipState();
+                }
+
+                if(board.checkWin()) {
+                    setTimeout(function () {
+                        document.querySelector(".game-screen h2").style.display = "block";
+                    }, 100);
                 }
             });
         }
